02aa2f3ca97b5c8b9fa1ceb1757e4e2b3fed50cc
Lungu Roxana Mihaela <roxanamihaelalungu97@gmail.com>
2020-01-05 16:03:29 +0200

    add use unique entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
